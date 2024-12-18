89368d3cdbad77127cc88189f6fddbfa006edc01
Serhii <seleon000@gmail.com>
2024-12-18 13:44:25 +0100

    first commit

---
+++ src/main/java/com/in28minutes/rest/webservices/ranga_restful_web_services/RangaRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.rest.webservices.ranga_restful_web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangaRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangaRestfulWebServicesApplication.class, args);
+	}
+
+}
