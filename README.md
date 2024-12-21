f91190ef4251cf85307e58a9547e5807a966de5e
Serhii <seleon000@gmail.com>
2024-12-22 00:20:48 +0100

    [SEC-03] Step 18 (38). Content negotiation. Step 19 (39).Internationalization

--- README.md
+++ README.md
@@ -17,19 +17,28 @@ https://github.com/in28minutes/spring-microservices-v3/blob/main/02.restful-web-
 
 Talent Api Tester
 
-Step 10 (27). Add response status and location<br>
-Step 11 (29). Add error handling.<br>
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
-UserNotFoundException
+Step 10 (27). Add response status and location
 
-Step 12 (31)
-Code Changes for the Step - https://github.com/in28minutes/spring-microservices-v3/blob/main/02.restful-web-services/01-step-by-step-changes/v2.md#step-12
-ResponseEntityExceptionHandler::handleException()
+Step 11 (29). Add error handling.<br>
+- @ResponseStatus(code = HttpStatus.NOT_FOUND)
+- UserNotFoundException
+
+Step 12 (31).
+- Code Changes for the Step - https://github.com/in28minutes/spring-microservices-v3/blob/main/02.restful-web-services/01-step-by-step-changes/v2.md#step-12
+- ResponseEntityExceptionHandler::handleException()
 
 Step 14 (33). Add validation
-@Valid, @Size, @Past
-handleMethodArgumentNotValid()
-ex.getFieldError().getDefaultMessage()
+- @Valid, @Size, @Past
+- handleMethodArgumentNotValid()
+- ex.getFieldError().getDefaultMessage()
 
 Step 17 (37). Swagger documentation (Open Api)
-http://localhost:8080/swagger-ui/index.html
+- http://localhost:8080/swagger-ui/index.html
+
+Step 18 (38). Content negotiation
+- pom.xml `<artifactId>jackson-dataformat-xml</artifactId>`
+- Accept header (MIME types - application/xml or application/json, 2 different representation of the response)
+
+Step 19 (39). Internationalization
+- messages.properties, MessageSource
+- Accept-Language=fr
--- src/main/java/com/in28minutes/rest/webservices/ranga_restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/ranga_restful_web_services/helloworld/HelloWorldController.java
@@ -1,15 +1,25 @@
 package com.in28minutes.rest.webservices.ranga_restful_web_services.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
 
+    private final MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
-        return "Hello World!1111221";
+        return "Hello World!11";
     }
 
     @GetMapping(path = "/hello-world-bean")
@@ -20,6 +30,16 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean("Hello World " + name);
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+//		- Example: `en` - English (Good Morning)
+//		- Example: `nl` - Dutch (Goedemorgen)
+//		- Example: `fr` - French (Bonjour)
+//		- Example: `de` - Deutsch (Guten Morgen)
     }
 
 }
